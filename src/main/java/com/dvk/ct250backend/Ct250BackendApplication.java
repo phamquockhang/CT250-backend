ae88d50e60ca337f48041b88bf521f5493d986c4
Tang Thien Dat <thiendattang@gmail.com>
2024-08-22 17:54:34 +0700

    init

---
+++ src/main/java/com/dvk/ct250backend/Ct250BackendApplication.java
@@ -0,0 +1,13 @@
+package com.dvk.ct250backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ct250BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ct250BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dvk/ct250backend/Ct250BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvk.ct250backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ct250BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
