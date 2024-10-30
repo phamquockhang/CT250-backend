686a9fdd4ad5b612006a859f28e146cba44808d8
Tang Thien Dat <thiendattang@gmail.com>
2024-10-30 09:52:31 +0700

    feat: add env variable loader

--- src/main/java/com/dvk/ct250backend/Ct250BackendApplication.java
+++ src/main/java/com/dvk/ct250backend/Ct250BackendApplication.java
@@ -1,5 +1,6 @@
 package com.dvk.ct250backend;
 
+import com.dvk.ct250backend.infrastructure.config.context.DotEnvApplicationContextInitializer;
 import com.dvk.ct250backend.infrastructure.config.security.RSAKeyRecord;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,7 +13,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableConfigurationProperties(RSAKeyRecord.class)
 public class Ct250BackendApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        SpringApplication.run(Ct250BackendApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(Ct250BackendApplication.class);
+        springApplication.addInitializers(new DotEnvApplicationContextInitializer());
+        springApplication.run(args);
     }
 
 }
